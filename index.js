88ddb23f6f95bf19007297eb05e624b4ff945d97
StefanPDavis <snoodle22@hotmail.com>
2024-01-23 15:50:17 -0800

    added question prompts

--- index.js
+++ index.js
@@ -2,3 +2,37 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 const {Circle, Triangle, Square} = require("./lib/shapes");
+
+function promptUser() {
+    inquirer
+      .prompt([
+        {
+        name: "text",
+        type: "input",
+        message: "Type in up to three characters for your logo",
+        },
+
+        {
+        name: "textColor",
+        type: "input",
+        message: "Type what color you want the text to be",
+        },
+
+        {
+        name: "shape",
+        type: "list",
+        message: "Select which shape you want in your logo",
+        choices: ["Circle", "Triangle", "Square"],
+        },
+
+        {
+        name: "shapeColor",
+        type: "input",
+        message: "Type a color or hexadecimal for your shape",
+        },
+
+      ])
+      
+};
+
+promptUser();
